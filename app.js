71f44cbc1397076da9bd0cb42d98d4139b4ddc24
anuptachamo <anup.tachamo12@gmail.com>
2024-02-03 17:08:54 +0545

    add hint in game to make it more user friendly

--- app.js
+++ app.js
@@ -19,8 +19,12 @@ while(true){
     if (guess == random){
         console.log("You are right ! congrats!! random number was", random);
         break;
+    }else if(guess < random){
+        guess= prompt("hint : Your guess was too small. please try again")
     }else{
-        guess = prompt("Your guess was wrong. please try again")
+        guess= prompt(" hint :Your guess was too large. please try again")
+
     }
+
 }
 
